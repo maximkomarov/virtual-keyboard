649e9fd5871638ba109bd82ba7b9c07b7a1b57d6
Maksim Kamarou <maksim.komarov01@gmail.com>
2022-05-09 18:00:19 +0300

    feat: general functionality added

--- keyboard.js
+++ keyboard.js
@@ -1,76 +1,136 @@
-const ASCIIarray = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8, 9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 160, 221, 92, 46,
-    20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 59, 222, 13, 16, 92, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 38, 16,
-    17, 91, 18, 32, 18, 17, 37, 40, 39];
+    const ASCIIarrayEnglish = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8, 9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220, 46, 
+      20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13, 16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 38,  91, 18, 32,  37, 40, 39, 17];
+    const ASCIIarrayRussian = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8, 9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220, 46,
+       20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13, 16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 38,  91, 18, 32, 37, 40, 39, 17];
     const englishLayout = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u',
     'i', 'o', 'p', '[', ']', '\\', 'Del', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n',
-    'm', ',', '.', '/', '&#8593', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '&#8592', '&#8595', '&#8594', 'Ctrl'];
+    'm', ',', '.', '/', '&#8593', 'Win', 'Alt', 'Space', '&#8592', '&#8595', '&#8594', 'Ctrl'];
     const englishLayoutUpperCase = englishLayout.map(element => element.toUpperCase());
     const englishShiftLayout = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u',
     'i', 'o', 'p', '{', '}', '|', 'Del', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '"', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n',
-    'm', '<', '>', '?', '&#8593', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '&#8592', '&#8595', '&#8594', 'Ctrl'];
+    'm', '<', '>', '?', '&#8593',  'Win', 'Alt', 'Space', '&#8592', '&#8595', '&#8594', 'Ctrl'];
     const englishShiftLayoutUpperCase = englishShiftLayout.map(element => element.toUpperCase());
     const russianLayout = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г',
     'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т',
-    'ь', 'б', 'ю', '.', '&#8593', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '&#8592', '&#8595', '&#8594', 'Ctrl'];
+    'ь', 'б', 'ю', '.', '&#8593', 'Win', 'Alt', 'Space', '&#8592', '&#8595', '&#8594', 'Ctrl'];
     const russianLayoutUpperCase = russianLayout.map(element => element.toUpperCase());
     const russianShiftLayout = ['~', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г',
     'ш', 'щ', 'з', 'х', 'ъ', '/', 'Del', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т',
-    'ь', 'б', 'ю', ',', '&#8593', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '&#8592', '&#8595', '&#8594', 'Ctrl'];
+    'ь', 'б', 'ю', ',', '&#8593', 'Win', 'Alt', 'Space', '&#8592', '&#8595', '&#8594', 'Ctrl'];
     const russianShiftLayoutUpperCase = russianShiftLayout.map(element => element.toUpperCase());
 
+import * as actions from './actions.js';
+
 export class Keyboard {
-    constructor() {
+    ASCIIarray = []
+    layout = []
+    language = 'en'
+    actions = new actions.Actions(); 
+    constructor(language) {
+      this.language = language
+        if (language === 'en'){
+            this.ASCIIarray = ASCIIarrayEnglish;
+            this.layout = englishLayout;
+        }
+        if (language === 'ru'){
+          this.ASCIIarray = ASCIIarrayRussian;
+          this.layout = russianLayout;
+        }
         let bodyCode = '<textarea id="input" autofocus></textarea><div id="keyboard">';
     
-        for (let i = 0; i < 64; i++) {
-          bodyCode += `<div class="button" onclick="keyClick(this)" id = "${ASCIIarray[i]}">
-          <span id="text">${englishLayout[i]}</span></div>`;
+        for (let i = 0; i < 61; i++) {
+          bodyCode += `<div class="button" style ="user-select:none" id = "${this.ASCIIarray[i]}">
+         ${this.layout[i]}</div>`;
         }
     
-        bodyCode += '</div>';
+        bodyCode += '</div><div>Создано под операционной системой Windows. <br> Для смены языка Shift + Alt.</div>'
         document.body.innerHTML = bodyCode;
       }
     
       keysPressed = [];
     
-      keyDown = function (e) {
+      keyDown(e) {
         if (e.repeat === true) {
           return;
         }
-        if (!ASCIIarray.includes(e.keyCode)) {
+        if (!this.ASCIIarray.includes(e.keyCode)) {
           return;
         }
         const code = e.keyCode;
-        console.log(e.keyCode);
         document.getElementById(`${code}`).classList.add('button-pressed');
-        keyBoard.keysPressed.push(code);
+        this.keysPressed.push(code);
+        if (document.getElementById(`${code}`).innerText.length === 1) {
+          this.keyClick(document.getElementById(`${code}`).innerText, input.selectionStart);
+        }
       };
     
-      keyUp = function (e) {
+      keyUp(e) {
         if (e.repeat === true) {
           return;
         }
-        if (!ASCIIarray.includes(e.keyCode)) {
+        if (!this.ASCIIarray.includes(e.keyCode)) {
           return;
         }
         const code = e.keyCode;
         document.getElementById(`${code}`).classList.remove('button-pressed');
-        keyBoard.checkCombination();
-        keyBoard.keysPressed = keyBoard.keysPressed.filter((element) => element !== code);
+        let result = this.checkCombination();
+        this.keysPressed = this.keysPressed.filter((element) => element !== code);
+        return result;
       };
-    
-      checkCombination = function () {
-        const combination = keyBoard.keysPressed.join('-');
-        switch (combination) {
-          case '16-18':
-            language === 'en' ? language = 'ru' : language = 'en';
-            localStorage.setItem('language', language);
-            keyBoard.updateLanguage();
+
+      keyClick(element, selectionStart){      
+        switch (element){
+          case 'Space':
+            this.actions.printCharacter(' ', selectionStart);
+            break;
+          case 'Backspace':
+            this.actions.backspace(selectionStart);
+            break;
+          case 'Tab':
+            this.actions.printCharacter('\t', selectionStart);
+            break;
+          case 'Del':
+            this.actions.del(selectionStart);
+            break;
+          case 'Enter':
+            this.actions.printCharacter('\n', selectionStart);
+            break;
+          case 'Ctrl':
+            return;
+          case 'Alt':
+            return;
+          case 'Win':
+            return;
+          case 'Shift':
+            return;
+          case 'CapsLock':
+            return;
+          case '→':
+            this.actions.arrowRight(selectionStart);
+            break;
+          case '←':
+            this.actions.arrowLeft(selectionStart);
+            break;
+          case '↓':
+            this.actions.arrowDown(selectionStart);
+            break;
+          case '↑':
+            this.actions.arrowUp(selectionStart);
+            break;
+          default:
+            this.actions.printCharacter(element, selectionStart);
             break;
         }
-      };
+        
+      }
     
-      updateLanguage = function () {
-        keyBoard = new KeyBoard();
+      checkCombination() {
+        const combination = this.keysPressed.join('-');
+        switch (combination) {
+          case '16-18':
+            this.language === 'en' ? this.language = 'ru' : this.language = 'en';
+            localStorage.setItem('language', this.language);
+            return this.language;
+        }
       };
 }
